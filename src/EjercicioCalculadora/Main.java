5072144cc1cb7dffbd297adccd437133acc663fd
jessicaHernandez <jessica.herriv@1DAW-4.iesluisbraille.es>
2025-02-11 10:27:58 +0100

    Agregue nuevo comentario

--- src/EjercicioCalculadora/Main.java
+++ src/EjercicioCalculadora/Main.java
@@ -50,5 +50,7 @@ public class Main {
         System.out.println("Modulo de la división: " + calcu2.modulo(num1, num2));
         System.out.println("Potencia: " + calcu2.potencia(num1, num2));
         System.out.println("***********************************************************************");
+        
+        System.out.println("Nuevo comentario");
     }
 }
